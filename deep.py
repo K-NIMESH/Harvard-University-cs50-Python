44c1d75f5a27518d96d32feb1346e500bd0e11cb
Alan Jordan <35063430+alanbjordan@users.noreply.github.com>
2022-06-01 09:36:13 -0400

    Update deep.py

--- deep.py
+++ deep.py
@@ -1,16 +1,16 @@
 def main():
-    q = input("What's the answer to the Great Question of Life, the Universe and Everything? ").lower().strip()
-    if greatAnswer(q):
+    userInput = input("What's the answer to the Great Question of Life, the Universe and Everything? ").lower().strip()
+    if greatAnswer(userInput):
         print("Yes")
     else:
         print("No")
 
-def greatAnswer(a):
-    if a == "42":
+def greatAnswer(userInput):
+    if userInput == "42":
         return True
-    elif "forty-two" in a:
+    elif "forty-two" in userInput:
         return True
-    elif "forty two" in a:
+    elif "forty two" in userInput:
         return True
 
 main()

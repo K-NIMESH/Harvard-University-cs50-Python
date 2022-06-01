4bd778f0dddcde02810608ac732ba44ef56b3621
Alan Jordan <35063430+alanbjordan@users.noreply.github.com>
2022-06-01 09:33:35 -0400

    Update bank.py

--- bank.py
+++ bank.py
@@ -1,11 +1,11 @@
 def main():
-    g = input("Please Enter A Greeting: ").lower().replace(",", "")
-    saidHello(g)
+    greeting = input("Please Enter A Greeting: ").lower().replace(",", "")
+    costOfGreeting(greeting)
 
-def saidHello(t):
-    if t.split()[0] == "hello" and "hello" in t:
+def costOfGreeting(greeting):
+    if greeting.split()[0] == "hello" and "hello" in greeting:
         print("$0")
-    elif t.index("h") == 0:
+    elif greeting.index("h") == 0:
         print("$20")
     else:
         print("$100")

024e8a27027610b21638795f905910008f0b82bc
Alan Jordan <35063430+alanbjordan@users.noreply.github.com>
2022-06-02 19:37:33 -0400

    Update coke.py

--- coke.py
+++ coke.py
@@ -7,12 +7,11 @@ def buy_coke():
     while price > 0:
         accepted_coins = [5, 10, 25]
         print("Amount Due:", price)
-        user_input = int(input("Insert Coin:"))
-        if user_input in accepted_coins:
-            price = price - user_input
-            if price <= 0:
-                refund = abs(price)
-                print("Change Owed: ", refund)
+        coin = int(input("Insert Coin:"))
+        if coin in accepted_coins:
+            price = price - coin
+    else:
+        print("Change Owed: ", abs(price))
 
 
 main()

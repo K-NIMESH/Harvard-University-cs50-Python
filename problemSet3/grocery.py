422118656af60c3ac041616ea624ac3836227340
Alan Jordan <35063430+alanbjordan@users.noreply.github.com>
2022-06-07 23:20:33 -0400

    Create grocery.py

---
+++ problemSet3/grocery.py
@@ -0,0 +1,20 @@
+g_list = []
+
+
+def main():
+    shop()
+
+
+def shop():
+    while True:
+        try:
+            user_input = input("").upper().strip()
+            g_list.append(user_input)
+        except (EOFError, KeyError):
+            sorted_list = sorted(set(g_list))
+            for i in sorted_list:
+                print(g_list.count(i),i)
+            quit()
+
+
+main()
